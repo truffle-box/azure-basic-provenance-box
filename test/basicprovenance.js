c7ae4c0ac009f14e2c53cfd945af7f07cfea1d3a
Ali Bhujwala <alibhujwala@gmail.com>
2019-11-08 14:25:11 -0800

    adding unit tests

---
+++ test/basicprovenance.js
@@ -0,0 +1,39 @@
+const BasicProvenance = artifacts.require("BasicProvenance")
+const truffleAssert = require('truffle-assertions');
+
+contract('BasicProvenance', accounts => {
+    const zeroAddress = '0x0000000000000000000000000000000000000000';
+    const owner = accounts[0];
+    const observer = accounts[1];
+    const new_owner = accounts[2];
+
+    it('should return a new instance of the contract with an observer', async() => {
+        const basicProvenance = await BasicProvenance.deployed();
+        const supplyChainOwner = await basicProvenance.Counterparty();
+        const supplyChainObserver = await basicProvenance.SupplyChainObserver();
+
+        assert.equal(supplyChainOwner, owner, 'owner not correctly set');
+        assert.equal(supplyChainObserver, observer, 'observer not correctly set');
+    });
+
+    it('should allow transferability of', async() => {
+        const basicProvenance = await BasicProvenance.deployed();
+
+        await basicProvenance.TransferResponsibility(new_owner);
+        const newCounterParty = await basicProvenance.Counterparty(); 
+
+        assert.equal(newCounterParty, new_owner, 'responsibility not transferred successfully');
+    });
+
+    it('should be only usable when not completed', async() => {
+        const basicProvenance = await BasicProvenance.deployed();
+
+        await basicProvenance.Complete();
+
+        await truffleAssert.reverts(basicProvenance.TransferResponsibility(owner));
+    })
+
+
+
+
+});
